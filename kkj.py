336f84e7fc2ebe3f0a5d37688b9b1b2f35946121
diegorkkj <diegosenaisp@gmail.com>
2023-07-27 08:18:00 -0300

    upload kkj

---
+++ kkj.py
@@ -0,0 +1,18 @@
+x = "n"
+
+while x !="y":
+
+    print ("─────█─▄▀█──█▀▄─█──")
+    print ("────▐▌──────────▐▌──")
+    print ("────█▌▀▄──▄▄──▄▀▐█──")
+    print ("───▐██──▀▀──▀▀──██▌──")
+    print ("──▄████▄──▐▌──▄████▄─")
+    print ("QUER PERDER O CHIP MLK??????????????????")
+
+    x = input("Deseja sair? \n y = SIM \n n = NÃO \n")
+    if x == "Y" or x == "sim":
+        x = "y"
+    elif x == "N" or x == "não":
+        x = "n"
+
+
